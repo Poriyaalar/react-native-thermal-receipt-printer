89c0fd3e6d47b020e85f8d6474c11e87e16be949
Karthirajan93 <106950862+Karthirajan93@users.noreply.github.com>
2025-03-07 12:09:51 +0530

    fix ble adapter:

--- android/src/main/java/com/pinmi/react/printer/adapter/BLEPrinterAdapter.java
+++ android/src/main/java/com/pinmi/react/printer/adapter/BLEPrinterAdapter.java
@@ -492,22 +492,21 @@ public class BLEPrinterAdapter implements PrinterAdapter {
             // printerOutputStream.write(SET_LINE_SPACE_24);
             printerOutputStream.write(CENTER_ALIGN);
 
-            int y = pixels.length;
-            // for (int y = 0; y < pixels.length; y += 24) {
-            // Like I said before, when done sending data,
-            // the printer will resume to normal text printing
-            printerOutputStream.write(SELECT_BIT_IMAGE_MODE);
-            // Set nL and nH based on the width of the image
-            printerOutputStream.write(
-                    new byte[] { (byte) (0x00ff & pixels[y].length), (byte) ((0xff00 & pixels[y].length) >> 8) });
-            for (int x = 0; x < pixels[y].length; x++) {
-                // for each stripe, recollect 3 bytes (3 bytes = 24 bits)
-                printerOutputStream.write(recollectSlice(y, x, pixels));
-            }
+            for (int y = 0; y < pixels.length; y += 24) {
+                // Like I said before, when done sending data,
+                // the printer will resume to normal text printing
+                printerOutputStream.write(SELECT_BIT_IMAGE_MODE);
+                // Set nL and nH based on the width of the image
+                printerOutputStream.write(
+                        new byte[] { (byte) (0x00ff & pixels[y].length), (byte) ((0xff00 & pixels[y].length) >> 8) });
+                for (int x = 0; x < pixels[y].length; x++) {
+                    // for each stripe, recollect 3 bytes (3 bytes = 24 bits)
+                    printerOutputStream.write(recollectSlice(y, x, pixels));
+                }
 
-            // Do a line feed, if not the printing will resume on the same line
-            printerOutputStream.write(LINE_FEED);
-            // }
+                // Do a line feed, if not the printing will resume on the same line
+                printerOutputStream.write(LINE_FEED);
+            }
             // printerOutputStream.write(SET_LINE_SPACE_32);
             // printerOutputStream.write(LINE_FEED);
 
